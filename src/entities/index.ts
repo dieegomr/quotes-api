85b958767198312a52befea87c65b736df5fb5f9
dieegomr <dmendes.rocha@gmail.com>
2023-02-24 09:48:42 -0300

    "feat: add userProfile type"

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './name';
 export * from './user';
 export * from './password';
+export * from './profile';
---
+++ src/entities/profile.ts
@@ -0,0 +1,6 @@
+export type UserProfile = {
+  name: string;
+  email: string;
+  createdQuotesQuantity: number;
+  likedQuotesQuantity: number;
+};
