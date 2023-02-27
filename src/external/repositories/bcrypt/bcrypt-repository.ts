980eba31affc1fa554e52e5ae3dacff3b0dc2321
dieegomr <dmendes.rocha@gmail.com>
2023-02-27 16:35:13 -0300

    "refactor: add PasswordHashing interface To BcryptRepository"

--- src/external/repositories/bcrypt/bcrypt-repository.ts
+++ src/external/repositories/bcrypt/bcrypt-repository.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt';
+import { PasswordHashing } from '../../../gateways';
 
-export class BcryptRepository {
-  async hashPassword(password: string): Promise<string> {
+export class BcryptRepository implements PasswordHashing {
+  async hash(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     return hashedPassword;
