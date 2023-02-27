86b6222bce7ba96622dace3c6fbdbaac75b9376b
dieegomr <dmendes.rocha@gmail.com>
2023-02-27 16:43:56 -0300

    "refactor: add salt constant"

--- src/external/repositories/bcrypt/bcrypt-repository.ts
+++ src/external/repositories/bcrypt/bcrypt-repository.ts
@@ -3,8 +3,8 @@ import { PasswordHashing } from '../../../gateways';
 
 export class BcryptRepository implements PasswordHashing {
   async hash(password: string): Promise<string> {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const BCRYPT_SALT = 10;
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_SALT);
     return hashedPassword;
   }
 }
