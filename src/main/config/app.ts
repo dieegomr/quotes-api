4a958f0cd69cd66ad268b89311c230a84769d103
dieegomr <dmendes.rocha@gmail.com>
2023-03-01 09:51:07 -0300

    "feat: setup server"

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import { setupRoutes } from '../config/routes';
+import setupMiddleware from './middleware';
 
 import express from 'express';
 
 const app = express();
+setupMiddleware(app);
 setupRoutes(app);
 export default app;
