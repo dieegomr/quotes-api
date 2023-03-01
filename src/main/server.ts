ca85bfe8e7774a62ff26ae7ae69e3d8138e2b9ee
dieegomr <dmendes.rocha@gmail.com>
2023-03-01 09:55:50 -0300

    "feat: add main function to the server file"

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,18 @@
+import { config } from 'dotenv';
+
+import { MongoClient } from '../external/database/mongo';
 import app from './config/app';
 
-const port = process.env.PORT || 8000;
+const main = async () => {
+  config();
 
-app.listen(port, () =>
-  console.log(`server runnig at: http://localhost:${port}`)
-);
+  await MongoClient.connect();
+
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () =>
+    console.log(`server runnig at: http://localhost:${port}`)
+  );
+};
+
+main();
