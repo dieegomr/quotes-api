33e8fd029f4606fefc98004163a6e9dddedb6edd
dieegomr <dmendes.rocha@gmail.com>
2023-03-01 09:58:52 -0300

    "chore: remove unnecessary comment"

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -7,12 +7,10 @@ import { CreateUserUseCase } from '../../usecases';
 
 export default (router: Router): void => {
   router.post('/user', async (req, res) => {
-    // add try catch
     const passwordHashing = new BcryptRepository();
     const repo = new MongoUserRepository();
     const createUser = new CreateUserUseCase(repo, passwordHashing);
     const controller = new CreateUserController(createUser);
-
     const response = await controller.handle(req);
 
     res.status(response.statusCode).json(response.data);
