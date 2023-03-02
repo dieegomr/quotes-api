45d214f502e36fd83227e79f72f91a3db547c835
dieegomr <dmendes.rocha@gmail.com>
2023-03-02 15:55:52 -0300

    "feat: add authentication middleware"

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express';
+import { TokenGenerator, UserRepository } from '../gateways';
+
+export function authMiddleware(
+  userRepository: UserRepository,
+  tokenGenerator: TokenGenerator
+) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+      return res.status(400).json('No token');
+    }
+
+    const decodedJwt = await tokenGenerator.verify(token);
+
+    const user = await userRepository.findById(decodedJwt.id);
+
+    if (!user) {
+      return res.status(400).json('User not found');
+    }
+
+    req.user = user;
+
+    next();
+  };
+}
