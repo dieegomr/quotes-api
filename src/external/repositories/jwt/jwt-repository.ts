6774d7a71c1970f1c052a441b6e96aa4ed86ccac
dieegomr <dmendes.rocha@gmail.com>
2023-03-02 15:51:52 -0300

    "feat: add verify method to jwt repository"

--- src/external/repositories/jwt/jwt-repository.ts
+++ src/external/repositories/jwt/jwt-repository.ts
@@ -1,7 +1,10 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { TokenGenerator } from '../../../gateways';
 
 export class JwtRepository implements TokenGenerator {
+  async verify(token: string): Promise<jwt.JwtPayload> {
+    return jwt.verify(token, process.env.JWT_PASS ?? '') as JwtPayload;
+  }
   async sign(userId: string): Promise<string> {
     return jwt.sign({ id: userId }, process.env.JWT_PASS ?? '', {
       expiresIn: '24h',
--- src/gateways/token-generator.ts
+++ src/gateways/token-generator.ts
@@ -1,3 +1,6 @@
+import { JwtPayload } from 'jsonwebtoken';
+
 export interface TokenGenerator {
   sign: (userId: string) => Promise<string>;
+  verify: (token: string) => Promise<JwtPayload>;
 }
