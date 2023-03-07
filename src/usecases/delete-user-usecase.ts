977080c205fa7dbca1c33d3c93df55680a0edb5f
dieegomr <dmendes.rocha@gmail.com>
2023-03-07 08:58:48 -0300

    "refactor: add either to delete user usecase"

--- src/usecases/delete-user-usecase.ts
+++ src/usecases/delete-user-usecase.ts
@@ -1,8 +1,9 @@
 import { UserRepository } from '../gateways';
+import { Either } from '../shared';
 
 export class DeleteUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
-  async execute(userId: string): Promise<void> {
-    await this.userRepository.delete(userId);
+  async execute(userId: string): Promise<Either<Error, string>> {
+    return await this.userRepository.delete(userId);
   }
 }
