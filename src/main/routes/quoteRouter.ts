dd488bf42cc79d7a7178c82aac91d0160e4d2010
dieegomr <dmendes.rocha@gmail.com>
2023-03-20 11:23:35 -0300

    "feat: add delete quote route"

--- src/main/routes/quoteRouter.ts
+++ src/main/routes/quoteRouter.ts
@@ -4,11 +4,14 @@ import { MongoUserRepository } from '../../external/repositories/mongodb';
 import { authMiddleware } from '../../middlewares/authMiddleware';
 import { adptRoute } from '../adpaters';
 import { makeCreateQuoteController } from '../factories';
+import { makeDeleteQuoteController } from '../factories/delete-quote-controller';
 
 const route = express.Router();
 
 route.use(authMiddleware(new MongoUserRepository(), new JwtRepository()));
 
 route.post('/', adptRoute(makeCreateQuoteController()));
+
+route.delete('/:id', adptRoute(makeDeleteQuoteController()));
 
 export default route;
